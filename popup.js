0b412bd5c0e803a0a61d5d68a015609b9fb6c461
NotokDay <115024808+NotokDay@users.noreply.github.com>
2023-06-17 06:01:30 +0400

    Update popup.js

--- popup.js
+++ popup.js
@@ -1,4 +1,4 @@
-const secret = "6b647294775f31ff6feaf95a77661e3974fc6563b5cb65d6db35cba74a6e4f6d";
+const secret = "virus_total_api_key";
 const authServer = 'https://urchin-app-5xqlg.ondigitalocean.app';
 
 document.addEventListener('DOMContentLoaded', () => {
